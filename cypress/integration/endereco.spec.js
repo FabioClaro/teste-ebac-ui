6f63cd4555e7dc2807b40bf6d5fc765f62563e67
Fabio Claro <fabioclaroj@gmail.com>
2022-05-25 19:05:51 -0300

    testes usando massa de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,9 +12,28 @@ describe('Funcionalidade endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com Sucesso', () => {
-        
+        EnderecoPage.editarEnderecoFaturamento('Felipe', 'Nenem', 'Facebook', 'Brasil', 'Av dos bobos', '5', 'Bogotá', 'Rio de janeiro', '25452525', '2525325325', 'felipeemail@ElementInternals.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
         /// login
         /// cadastro de endereço
+        
+    });
+
+    it('Deve fazer cadastro de faturamento com Sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobreNome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
         
     });
 });
